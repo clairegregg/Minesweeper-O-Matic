9bd143ace9048d22036faffbca82ab855bba200e
Claire Gregg <clairegregg@clairegregg.com>
2023-12-22 14:40:48 +0000

    Organise and add comments

--- src/Model.hs
+++ src/Model.hs
@@ -24,62 +24,10 @@ play g = case findObviousBomb (0, 0) g of
 
 applyPattern :: (Int, Int) -> Game -> Game
 applyPattern (x,y) g
-  | isValidIndex g (x,y) = if hasPattern1dashAll (x,y) g then flipSquare (x,y) g else applyPattern (0, y+1) g
+  | isValidIndex g (x,y) = if hasPattern1dash1All (x,y) g then flipSquare (x,y) g else applyPattern (0, y+1) g
   | isValidIndex g (0, y+1) = applyPattern (0, y+1) g
   | otherwise = g
 
-
-hasPattern1dashAll :: (Int, Int) -> Game -> Bool
-hasPattern1dashAll (x,y) g = hasPattern1dash1Top (x,y) g || hasPattern1dash1Bottom (x,y) g || hasPattern1dashLeft (x,y) g || hasPattern1dashRight (x,y) g
-
-hasPattern1dash1Top :: (Int, Int) -> Game -> Bool
-hasPattern1dash1Top (x,y) g = hasPattern1dash tilesAgainstTopEdge [(x,y-2),(x-1,y-2)] [(x-1, y-2),(x-1,y-1)] g
-                            || hasPattern1dash tilesAgainstTopEdge [(x,y-2),(x+1,y-2)] [(x+1, y-2),(x+1,y-1)] g
-
-hasPattern1dash1Bottom :: (Int, Int) -> Game -> Bool
-hasPattern1dash1Bottom (x,y) g = hasPattern1dash tilesAgainstBottomEdge [(x,y+2),(x-1,y+2)] [(x-1, y+2),(x-1,y+1)] g
-                            || hasPattern1dash tilesAgainstTopEdge [(x,y+2),(x+1,y+2)] [(x+1, y+2),(x+1,y+1)] g
-
-
-hasPattern1dashLeft :: (Int,Int) -> Game -> Bool
-hasPattern1dashLeft (x,y) g = hasPattern1dash tilesAgainstLeftEdge [(x-2,y),(x-2,y+1)] [(x-2, y+1),(x-1,y+1)] g
-                                || hasPattern1dash tilesAgainstLeftEdge [(x-2,y),(x-2,y-1)] [(x-2, y-1),(x-1,y-1)] g
-
-hasPattern1dashRight :: (Int,Int) -> Game -> Bool
-hasPattern1dashRight (x,y) g = hasPattern1dash tilesAgainstRightEdge [(x+2,y),(x+2,y+1)] [(x+2, y+1),(x+1,y+1)] g
-                                || hasPattern1dash tilesAgainstRightEdge [(x+2,y),(x+2,y-1)] [(x+2, y-1),(x+1,y-1)] g
-
-hasPattern1dash :: ([(Int,Int)] -> Game -> Bool) -> [(Int,Int)] -> [(Int,Int)] -> Game -> Bool
-hasPattern1dash edgeChecker edgeTiles tilesToBeEmpty1 g = (validPosition && edgeChecker edgeTiles g) && all tileIsEmpty1 tilesToBeEmpty1
-                            where
-                                validPosition :: Bool
-                                validPosition = all (isValidIndex g) edgeTiles
-
-                                tileIsEmpty1 :: (Int,Int) -> Bool
-                                tileIsEmpty1 c' = case getTile g c' of
-                                                    (Revealed (Empty 1)) -> True
-                                                    _ -> False
-
-
--- This checks if all of a list of tiles are next to "an edge" (for pattern matching)
--- This can be either if they are against the edge of the grid, or if they are all next to revealed tiles
-tilesAgainstTopEdge :: [(Int, Int)] -> Game -> Bool
-tilesAgainstTopEdge = tilesAgainstEdge (0,-1)
-
-tilesAgainstBottomEdge :: [(Int, Int)] -> Game -> Bool
-tilesAgainstBottomEdge = tilesAgainstEdge (0,1)
-
-tilesAgainstLeftEdge :: [(Int,Int)] -> Game -> Bool
-tilesAgainstLeftEdge = tilesAgainstEdge (-1,0)
-
-tilesAgainstRightEdge :: [(Int,Int)] -> Game -> Bool
-tilesAgainstRightEdge = tilesAgainstEdge (1,0)
-
-tilesAgainstEdge :: (Int,Int) -> [(Int,Int)] -> Game -> Bool
-tilesAgainstEdge _ [] _ = True
-tilesAgainstEdge (dx,dy) ((x,y):cs) g = if isValidIndex g (x+dx, y+dy)
-                                        then isRevealed (getTile g (x+dx, y+dy)) && tilesAgainstEdge (dx,dy) cs g
-                                        else tilesAgainstEdge (dx,dy) cs g
 
 -- This is the equivalent of the basic patterns discussed in https://www.minesweeper.info/wiki/Strategy
 -- Loops through all of the tiles in the map, and if there are exactly as many unrevealed spots adjacent as mines predicted next to a revealed spot, 
@@ -92,31 +40,6 @@ findObviousBomb (x,y) g
                             Just c -> Just c
   | isValidIndex g (0, y+1) = findObviousBomb (0, y+1) g
   | otherwise = Nothing
-
--- Given a tile in the grid
---  - Check if it is a revealed empty tile (if not, return)
---  - Store the number of bombs next to the tile (from its value) in adjacentBombs
---  - Count the number of adjacent tiles which are unrevealed
---  - If adjacentBombs == adjacentUnrevealed, then all of the unrevealed tiles are bombs
---  - Find an unflagged bomb from the adjaacent tiles, and return its index. 
---  - If no bomb is found (because they are all flagged), return Nothing
-findBombAdjacentToTile :: (Int,Int) -> Game -> Maybe (Int,Int)
-findBombAdjacentToTile c g = do
-                                adjacentBombs <- case getTile g c of
-                                                Revealed (Empty 0) -> Nothing
-                                                Revealed (Empty x) -> Just x
-                                                _ -> Nothing
-                                let adjacentUnrev = countAdjecentUnrevealed c g
-                                if adjacentBombs == adjacentUnrev
-                                    then findBomb (adjacentIndices c g) g
-                                    else Nothing
-
--- Given a list of tile indices
---   where any unrevealed tiles are bombs,
---   return one of any unflipped tile indices (ignoring flipped tiles)
-findBomb :: [(Int,Int)] -> Game -> Maybe (Int, Int)
-findBomb [] _ = Nothing
-findBomb (c:cs) g = if isUnflipped (getTile g c) then Just c else findBomb cs g
 
 -- Takes in the map and coordinates of a square, and flags it.
 flagSquare :: (Int, Int) -> Game ->  Game
@@ -194,6 +117,106 @@ checkFlipGameCondition (m,g) (x,y)
       checkSquareCorrect (Flagged Mine) = True
       checkSquareCorrect (Revealed (Empty _)) = True -- An empty tile must be revealed for the game to be won.
       checkSquareCorrect _ = False -- Any other case means the game has not been won yet.
+
+{- Simplest bomb finding -}
+
+-- Given a tile in the grid
+--  - Check if it is a revealed empty tile (if not, return)
+--  - Store the number of bombs next to the tile (from its value) in adjacentBombs
+--  - Count the number of adjacent tiles which are unrevealed
+--  - If adjacentBombs == adjacentUnrevealed, then all of the unrevealed tiles are bombs
+--  - Find an unflagged bomb from the adjaacent tiles, and return its index. 
+--  - If no bomb is found (because they are all flagged), return Nothing
+findBombAdjacentToTile :: (Int,Int) -> Game -> Maybe (Int,Int)
+findBombAdjacentToTile c g = do
+                                adjacentBombs <- case getTile g c of
+                                                Revealed (Empty 0) -> Nothing
+                                                Revealed (Empty x) -> Just x
+                                                _ -> Nothing
+                                let adjacentUnrev = countAdjecentUnrevealed c g
+                                if adjacentBombs == adjacentUnrev
+                                    then findBomb (adjacentIndices c g) g
+                                    else Nothing
+
+-- Given a list of tile indices
+--   where any unrevealed tiles are bombs,
+--   return one of any unflipped tile indices (ignoring flipped tiles)
+findBomb :: [(Int,Int)] -> Game -> Maybe (Int, Int)
+findBomb [] _ = Nothing
+findBomb (c:cs) g = if isUnflipped (getTile g c) then Just c else findBomb cs g
+
+{- 1-1 pattern finding, for when there are two revealed Empty 1s next to each other against a wall:
+    with | as a wall, [ ] as unrevealed, we can correctly flag where [F] is shown
+  | [ ] [ ] [F]
+  | [1] [1] 
+
+  This works in a number of orientations. All of these functions take in potential [F] tiles, and return if the 1-1 rule would allow them to be flipped safely
+ -}
+
+-- Check all possible orientations of the 1-1 pattern for a given tile, return true if it matches the pattern.
+hasPattern1dash1All :: (Int, Int) -> Game -> Bool
+hasPattern1dash1All (x,y) g = hasPattern1dash1Top (x,y) g || hasPattern1dash1Bottom (x,y) g || hasPattern1dash1Left (x,y) g || hasPattern1dash1Right (x,y) g
+
+-- Check the 2 possible orientations for the 1-1 pattern where the edge is at the top.
+hasPattern1dash1Top :: (Int, Int) -> Game -> Bool
+hasPattern1dash1Top (x,y) g = hasPattern1dash1 tilesAgainstTopEdge [(x,y-2),(x-1,y-2)] [(x-1, y-2),(x-1,y-1)] g
+                            || hasPattern1dash1 tilesAgainstTopEdge [(x,y-2),(x+1,y-2)] [(x+1, y-2),(x+1,y-1)] g
+
+-- Check the 2 possible orientations for the 1-1 pattern where the edge is at the bottom.
+hasPattern1dash1Bottom :: (Int, Int) -> Game -> Bool
+hasPattern1dash1Bottom (x,y) g = hasPattern1dash1 tilesAgainstBottomEdge [(x,y+2),(x-1,y+2)] [(x-1, y+2),(x-1,y+1)] g
+                            || hasPattern1dash1 tilesAgainstTopEdge [(x,y+2),(x+1,y+2)] [(x+1, y+2),(x+1,y+1)] g
+
+-- Check the 2 possible orientations for the 1-1 pattern where the edge is at the left.
+hasPattern1dash1Left :: (Int,Int) -> Game -> Bool
+hasPattern1dash1Left (x,y) g = hasPattern1dash1 tilesAgainstLeftEdge [(x-2,y),(x-2,y+1)] [(x-2, y+1),(x-1,y+1)] g
+                                || hasPattern1dash1 tilesAgainstLeftEdge [(x-2,y),(x-2,y-1)] [(x-2, y-1),(x-1,y-1)] g
+
+-- Check the 2 possible orientations for the 1-1 pattern where the edge is at the right.
+hasPattern1dash1Right :: (Int,Int) -> Game -> Bool
+hasPattern1dash1Right (x,y) g = hasPattern1dash1 tilesAgainstRightEdge [(x+2,y),(x+2,y+1)] [(x+2, y+1),(x+1,y+1)] g
+                                || hasPattern1dash1 tilesAgainstRightEdge [(x+2,y),(x+2,y-1)] [(x+2, y-1),(x+1,y-1)] g
+
+-- Generalised case for the 1-1 pattern
+-- Given
+--   - A function which takes in a list of tile indices and returns true if they are against the appropriate edge
+--   - A list of tiles which must be against the edge
+--   - A list of tiles which must be Revealed (Empty 1)
+--   - The Game
+-- Checks if the appropriate tiles are against the edge, and the appropriate tiles are Revealed (Empty 1), and returns this.
+hasPattern1dash1 :: ([(Int,Int)] -> Game -> Bool) -> [(Int,Int)] -> [(Int,Int)] -> Game -> Bool
+hasPattern1dash1 edgeChecker edgeTiles tilesToBeEmpty1 g = (validPosition && edgeChecker edgeTiles g) && all tileIsEmpty1 tilesToBeEmpty1
+                            where
+                                validPosition :: Bool
+                                validPosition = all (isValidIndex g) edgeTiles
+
+                                tileIsEmpty1 :: (Int,Int) -> Bool
+                                tileIsEmpty1 c' = case getTile g c' of
+                                                    (Revealed (Empty 1)) -> True
+                                                    _ -> False
+
+
+-- These functions all check if all of a list of tiles are next to "an edge" (for pattern matching)
+-- This can be either if they are against the edge of the grid, or if they are all next to revealed tiles
+tilesAgainstTopEdge :: [(Int, Int)] -> Game -> Bool
+tilesAgainstTopEdge = tilesAgainstEdge (0,-1)
+
+tilesAgainstBottomEdge :: [(Int, Int)] -> Game -> Bool
+tilesAgainstBottomEdge = tilesAgainstEdge (0,1)
+
+tilesAgainstLeftEdge :: [(Int,Int)] -> Game -> Bool
+tilesAgainstLeftEdge = tilesAgainstEdge (-1,0)
+
+tilesAgainstRightEdge :: [(Int,Int)] -> Game -> Bool
+tilesAgainstRightEdge = tilesAgainstEdge (1,0)
+
+-- This is the generalised function for checking if a list of tiles are all against an edge.
+-- The direction to go to find the edge being checked is given as (dx,dy).
+tilesAgainstEdge :: (Int,Int) -> [(Int,Int)] -> Game -> Bool
+tilesAgainstEdge _ [] _ = True
+tilesAgainstEdge (dx,dy) ((x,y):cs) g = if isValidIndex g (x+dx, y+dy)
+                                        then isRevealed (getTile g (x+dx, y+dy)) && tilesAgainstEdge (dx,dy) cs g
+                                        else tilesAgainstEdge (dx,dy) cs g
 
 {- HELPER FUNCTIONS -}
 -- Given a tile index, count how many adjacent tiles are currently unrevealed
